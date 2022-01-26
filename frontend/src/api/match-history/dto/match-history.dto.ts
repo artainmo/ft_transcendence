45add02149675e1cd551184a2634cbd008381c78
artainmo <tainmontarthur@icloud.com>
2022-01-26 15:39:30 +0100

    match-history db post functional

--- frontend/src/api/match-history/dto/match-history.dto.ts
+++ frontend/src/api/match-history/dto/match-history.dto.ts
@@ -7,3 +7,11 @@ export interface MatchHistoryDto {
   opponent_id: number;
   opponent_score: number;
 }
+
+export interface CompleteMatchHistoryDto {
+  id: number;
+  me: UserDto;
+  my_score: number;
+  opponent: UserDto;
+  opponent_score: number;
+}
